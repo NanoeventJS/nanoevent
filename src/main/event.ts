2fb2ef9725d631ba252254fdb117909f813f8ffd
Boris Okunskiy <boris@okunskiy.name>
2022-10-13 17:58:18 +0300

    feat: rewrite to multimap to improve removeAll perf

--- src/main/event.ts
+++ src/main/event.ts
@@ -1,47 +1,63 @@
 export type EventListener<T> = (event: T) => void;
 
-interface ListenerEntry<T> {
-    listener: EventListener<T>;
-    once: boolean;
-    group: any;
-}
-
-export type ExtractEventType<T extends Event<any>> = T extends Event<infer R> ? R : never;
+export type ListenerGroup<T> = Set<EventListener<T>>;
 
 export type EventSubscription = () => void;
 
 export class Event<T> {
-    private listeners: ListenerEntry<T>[] = [];
+    protected _groups = new Map<any, ListenerGroup<T>>();
+    protected _listenerToGroup = new Map<EventListener<T>, any>();
 
-    on(listener: EventListener<T>, group: any = undefined): EventSubscription {
-        this.listeners.push({ listener, once: false, group });
-        return () => this.off(listener);
+    on(listener: EventListener<T>, group: any = Symbol()): EventSubscription {
+        let set = this._groups.get(group);
+        if (!set) {
+            set = new Set();
+            this._groups.set(group, set);
+        }
+        set.add(listener);
+        this._listenerToGroup.set(listener, group);
+        return () => this.remove(group, listener);
     }
 
-    once(listener: EventListener<T>, group: any = undefined): EventSubscription {
-        this.listeners.push({ listener, once: true, group });
-        return () => this.off(listener);
+    once(listener: EventListener<T>, group: any = Symbol()): EventSubscription {
+        const wrapper: EventListener<T> = ev => {
+            listener(ev);
+            this.remove(group, wrapper);
+        };
+        return this.on(wrapper, group);
     }
 
     off(listener: EventListener<T>) {
-        const i = this.listeners.findIndex(_ => _.listener === listener);
-        if (i > -1) {
-            this.listeners.splice(i, 1);
+        const group = this._listenerToGroup.get(listener);
+        if (group) {
+            this.remove(group, listener);
         }
     }
 
+    protected remove(group: any, listener: EventListener<T>) {
+        const set = this._groups.get(group);
+        if (set) {
+            set.delete(listener);
+            if (set.size === 0) {
+                this._groups.delete(group);
+            }
+        }
+        this._listenerToGroup.delete(listener);
+    }
+
     removeAll(group: any) {
-        this.listeners = this.listeners.filter(_ => _.group !== group);
+        const set = this._groups.get(group);
+        this._groups.delete(group);
+        if (set) {
+            for (const listener of set) {
+                this._listenerToGroup.delete(listener);
+            }
+        }
     }
 
     emit(event: T) {
-        for (let i = 0; i < this.listeners.length; i++) {
-            const { listener, once } = this.listeners[i];
+        for (const listener of this._listenerToGroup.keys()) {
             listener(event);
-            if (once) {
-                this.listeners.splice(i, 1);
-                i -= 1;
-            }
         }
     }
 
